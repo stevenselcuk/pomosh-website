6efc3b6945755bd5339e6000f2b6d680a94c0f14
Steven J. Selcuk <stevenjselcuk@gmail.com>
2020-05-26 22:07:53 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+<div align="center">
+	<a href="https://garlic.netlify.app/" target="_blank">
+	<img src="https://imgur.com/pkggFcw.png" width="200" height="200">
+		<h1>garlic website</h1>
+	</a>
+	<p>
+		<b> built with Gatsby </b>
+	</p>
+	<br>
+	<br>
+	<br>
+</div>
+
+
+<p align="center">
+    <img
+      src="https://i.imgur.com/ZIRAbOj.png"
+      height="500"
+      alt="theGarlic"
+      title="garlic website"
+    />
+  </p>
+  
+ 
+  
---
+++ src/hooks/use-meta.js
@@ -0,0 +1,36 @@
+import { useStaticQuery, graphql } from 'gatsby'
+
+const query = graphql`
+  query Meta {
+    metaYaml {
+      description
+      bugs
+      repo
+      gatsbyThemePromotion
+      img {
+        childImageSharp {
+          resize(width: 980) {
+            src
+          }
+        }
+      }
+      productImg {
+        childImageSharp {
+          resize(width: 980) {
+            src
+          }
+        }
+      }
+      availability {
+        status
+        available
+        unavailable
+      }
+    }
+  }
+`
+
+export const useMeta = () => {
+  const { metaYaml } = useStaticQuery(query)
+  return metaYaml
+}
